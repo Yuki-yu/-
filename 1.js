3ddb9ea53992df7c78446dd2551007dcf5b5eefc
Yuki-yu <jianyu0939@163.com>
2018-05-13 21:53:16 +0800

    提交文件

---
+++ 1.js
@@ -0,0 +1,3 @@
+var r = require('./2.js');
+
+console.log(a)
---
+++ 2.js
@@ -0,0 +1,13 @@
+module.export = {
+    ac: function() {
+        var a = {},
+            b = {},
+            c = {};
+
+        a[b] = 123;
+        a[c] = 456;
+        console.log(555)
+        ac();
+    }
+
+}
